eabea1777d225950e60856c6bb675a2d1ea8f93f
Diana-KDD <dianaklimovich44@gmail.com>
2026-05-09 23:19:13 +0300

    Add actions admin

---
+++ WebsiteProgect/WebsiteProgect/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebsiteProgect.Data;
+using WebsiteProgect.Models;
+
+namespace WebsiteProgect.Controllers
+{
+    public class AdminController:Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(AppDbContext context, ILogger<AdminController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var places = await _context.Places
+                    .Include(p => p.Images)
+                    .Include(p => p.City)
+                        .ThenInclude(c => c.Country)
+                    .ToListAsync();
+
+            return View(places);
+        }
+    }
+}
