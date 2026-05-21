9968887a42d9320a8956ce93e32bb1f83353968f
Diana-KDD <dianaklimovich44@gmail.com>
2026-05-21 22:26:51 +0300

    add style Place Details

---
+++ WebsiteProgect/WebsiteProgect/wwwroot/js/PlaceDetails.js
@@ -0,0 +1,16 @@
+﻿/* === SWIPER НАСТРОЙКИ === */
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 500,
+    grabCursor: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
